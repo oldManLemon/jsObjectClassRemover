ad72187fd71a3b31ee652368cf57ef1ce5cdee50
hase <oldm8reggie@gmail.com>
2018-11-09 11:56:49 +1000

    Working for ID

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 //console.log("hello world");
-var turnoff ={
+/* var turnoff ={
     class: '',
     id: '',
     function(){
@@ -18,13 +18,55 @@ var turnoff ={
             console.log("ID: "+ id + " Class: "+ htmlClass);
         }
         
+    } 
+} 
+*/
+
+
+/* function removeID(){
+  var elem = document.getElementById('remove');
+  elem.style.display = 'none';
+} */
+
+function toggleRemoveal(elems){
+    if (elems.style.display === 'none') {
+        console.log('here')
+        elems.style.display = 'block';
+    }else{
+       elems.style.display = 'none'; 
     }
 }
 
+function Elementals(id, htmlClass) {
+    //easier way of hiding classes
+    //IF you want to use classes call it with var x = new Elementals(undefined, 'myClasses');
+    this.id = id;
+    this.class = htmlClass;
+    this.remove = function () {
+        if (this.class != undefined) {
+            elems = document.getElementsByClassName(this.class);
+            console.log("elems");
+        }
+        else if (this.id != undefined) {
+            elems = document.getElementById(this.id);
+            if (elems.style.display === 'none') {
+                console.log('here')
+                elems.style.display = 'block';
+            }else{
+               elems.style.display = 'none'; 
+            }
+            
 
-function removeID(){
-  var elem = document.getElementById('remove');
-  elem.style.display = 'none';
+        }
+        else {
+            id = document.getElementById(this.id);
+            htmlClass = document.getElementsByClassName(this.class);
+            console.log("ID: " + id + " Class: " + htmlClass);
+        }
+
+    }
 }
 
-var 
+var pink = new Elementals('remove');
+
+//pink.remove();
